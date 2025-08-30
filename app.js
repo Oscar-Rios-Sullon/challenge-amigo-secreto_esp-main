f8240f931e6d5d47ba9914e9a567f5c89399069a
Oscar Rios <oscarsullon1919@gmail.com>
2025-08-29 22:50:22 -0500

    actualizar correcciones en el app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let amigos = ["Ana", "Beto", "Carla", "Diego", "Elena"];
 function agregarAmigo() {
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
+
     if (nombre === "") {
         alert("Por favor, ingresa un nombre válido.");
         return;
@@ -17,24 +18,24 @@ function agregarAmigo() {
 }
 
 /* función para asignar amigos secretos */
-function mostrarListaDeAmigos(amigos) {
-    const Lista = document.getElementById("listaAmigos");
-
-    Lista.innerHTML = ""; // Limpiar la lista antes de mostrarla
+function mostrarListaDeAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista antes de mostrarla
 
     for (let i = 0; i < amigos.length; i++) {
         const li = document.createElement("li");
         li.textContent = amigos[i];
-        Lista.appendChild(li);
+        lista.appendChild(li);
     }
 }
 
 /* funcion para sortear los amigos */
 function sortearAmigo () {
     if (amigos.length === 0) {
-        alert("No hay amigos en la lista para sortear.");
+        alert("No hay amigos en la lista para sortear");
         return;
     }
+    
     const indiceAleatorio = Math.floor(Math.random() * amigos.length);
     const amigoSorteado = amigos[indiceAleatorio];
 
